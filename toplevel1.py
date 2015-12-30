f1246e040ed047f94bb6f8ae13cf9b567823af77
Bonan Zhu <zhubonan1992@gmail.com>
2015-12-30 11:47:19 +0000

    Added circular polarisation calculation functionality

--- toplevel1.py
+++ toplevel1.py
@@ -1,31 +1,35 @@
 # -*- coding: utf-8 -*-
 """
 Created on Sat Dec 19 22:45:56 2015
-
+Test codes
 @author: Bonan
 """
 
 import numpy as np
 import matplotlib.pyplot as plt
-from class_def import Layers, Material
+from multilayer import Layers,U_Material
 
 #%% Initialise the instance object
-a = [[200,300,1000], [1,1.2,1.5]]
-b = [[200,300,1000], [1,1.3,1.5]]
+a = [[200,300,1000], [2,2,2]]
+b = [[200,300,1000], [2.1,2.1,2.1]]
 m = U_Material(a,b)
-l = Layers(m, 200, 10, 1000)
+l = Layers(m, 600,50,2000)
 #%% calculation routine
 lam = []
-tss = []
-tsp = []
+rrr = []
+rll = []
 
 for wavelength in np.linspace(400,900, 100):
-    l.set_incidence([1,0,1], wavelength)
+    l.set_incidence([0,0,1], wavelength)
     l.doit()
-    tss.append(l.coeff_modulus["tss"])
-    tsp.append(l.coeff_modulus["tsp"])
+    rrr.append(l.coeff_modulus_LR["rpp"])
+    rll.append(l.coeff_modulus_LR["rss"])
     lam.append(wavelength)
 #%%
-plt.plot(lam,tss)
-plt.plot(lam,tsp)
+plt.plot(lam,rrr,label = "R-R reflectivity")
+plt.plot(lam,rll, label = "L-L reflectivity")
+plt.xlabel("Wavelength /nm")
+plt.ylabel("Reflectivity")
+plt.legend(loc = 2)
+plt.title("[0,0,1] incidence")
     

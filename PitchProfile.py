5729dcaaebb0835a407f883495eb990134894800
Bonan-Switch <zhubonan1992@gmail.com>
2016-02-01 15:12:20 +0000

    Added class StepPitchProfile that defines the pitch profile using numbers of steps. This allow sythesis of pitch profile from desired spectrum possible

--- PitchProfile.py
+++ PitchProfile.py
@@ -8,8 +8,15 @@ Function to compute the angluar profile of a generalised helix structure
 import numpy as np
 import scipy as sp
 from scipy.interpolate import interp1d
-
+import matplotlib.pyplot as pl
 class PitchProfile():
+    """A class represent the pitch profile of a generalised helix structure"""
+    
+    def getAngles():
+        raise NotImplementedError
+        
+        
+class AnyPitchProfile(PitchProfile):
     """A class represent the pitch profile of a generalised helix structure"""
     def __init__(self, pitchList, handness = 'left'):
         """Initialise by passing pitch profile and total thickness"""
@@ -31,7 +38,46 @@ class PitchProfile():
         zAngles = [sp.integrate.fixed_quad(self._angleFunction, 0, z)[0] for z in zList]
         return zAngles
 
+class StepPitchProfile(PitchProfile):
+    """Describe a pritch profile using a series steps"""
+    def __init__(self, stepLocation, stepSize, basePitch, totalThickness):
+        """Initialise the profile
+        
+        * stepList: 2*N list of [[z],[step]]. A adding a step moves segments on the two sides by -step,+step
+        
+        * basePitch: Basis pitch value
+        
+        * totalThickness: Total thickness of the profile
+        
+        """
+        self.stepLocation = np.array(stepLocation)
+        self.stepSize = np.array(stepSize)
+        self.basePitch = basePitch
+        self.totalThickness = totalThickness
+        self.calcDeltaList()
+        
+    def calcDeltaList(self):
+        """Calculate the change from basis for each segments"""
+        delta = []
+        lastDelta = 0
+        lastStep = 0
+        for i in range(self.stepSize.size):
+            lastDelta += lastStep*2 - self.stepSize[i]
+            delta.append(lastDelta)
+            lastStep = self.stepSize[i]
+        delta.append(lastDelta + lastStep*2) #added the delta of the final segment
+        self.delta = np.array(delta)
+    def getPitch(self, z):
+        
+        #Check if z lie on any point of steps
+        return self.delta[np.searchsorted(self.stepLocation, z)]+self.basePitch
+        
+    def plotPitch(self, n= 50):
+        
+        depth = np.linspace(0,self.totalThickness, n)
+        pl.plot(depth, self.getPitch(depth))
+        
 if __name__ == '__main__':
     
-    p = PitchProfile([[0,1000],[150,150]])
-    print(p.getAngles(np.linspace(0,1000,50)))
+    p = StepPitchProfile([500],[10],500,1000)
+    p.plotPitch()

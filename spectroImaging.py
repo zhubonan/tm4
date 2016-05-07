8693dbfe2f2054082517a44ba50f7d98fb2ccec0
Bonan Zhu <zhubonan1992@gmail.com>
2016-05-07 23:40:22 +0100

    Modified script for defect strcuture

--- spectroImaging.py
+++ spectroImaging.py
@@ -23,9 +23,9 @@ scan0502slim = r'C:\Users\Bonan\OneDrive\Documents\PartIII\Project\20160502\scan
 scan0427slim = r'C:\Users\Bonan\OneDrive\Documents\PartIII\Project\20160427\scanslim.mat'
 scan0408slim = r'C:\Users\Bonan\OneDrive\Documents\PartIII\Project\20160408\scanslim.mat'
 # %%Load Data
-test = mt.scanData(scan0408slim)
+test = mt.scanData(scan0427slim)
 #%% Preview
-stemp = test.getCurrentSpecData(15)
+stemp = test.getCurrentSpecData(5)
 stemp.cropSelf([450,750])
 stemp.setSpacialShape((51,51)) ## Define the shape of data
 stemp.testFilter(21,2)
@@ -33,8 +33,8 @@ stempcf = stemp.getFilteredSpectrum(21,2)
 stempcf.spec = stempcf.spec #Minus a backgroud
 stempcf.get2DColouredImage('auto',1)
 #%% Plot sliced data
-start , finish = (0,0),(25,25) #Deine positions
-num = 50 # Number of interpolation
+start , finish = (0,0),(50,50) #Deine positions
+num = 80 # Number of interpolation
 dist = np.linalg.norm(np.array(finish)-np.array(start))
 zi = stempcf.plotLineSlice(start, finish, num, show = 0)
 RGBImage = stempcf.get2DColouredImage()
@@ -57,14 +57,14 @@ pl.tight_layout(pad = 0.5)
 #pl.savefig(figPath + 'SpectroImagingSlice.pdf')
 #%% Plot some stats data
 peakData = stempcf.getPeaks()
-pl.figure()
+pl.figure(figsize = (3.3,2.8))
 pl.plot(peakData[0],peakData[1],'.')
 pl.xlabel('Wavelength /nm')
 pl.ylabel('Reflectance')
-pl.plot([540,710],[0.58,0.2],'k--', linewidth = 2)
+pl.plot([518,590],[0.90,0.55],'k--', linewidth = 2)
 pl.title('Height vs Position')
 arrow2 = dict(facecolor='black', headwidth = 5, width = 2,headlength = 10)
-pl.annotate('Cut-off', [610,0.45],[650,0.5],arrowprops = arrow2)
+pl.annotate('Cut-off', [540,0.8],[560,0.9],arrowprops = arrow2)
 pl.tight_layout(pad = 0.5)
 #pl.savefig(figPath + 'SpectroImagingStats.pdf')
 #%%Plot an example spectrum
@@ -76,7 +76,7 @@ m = sim.UniaxialMaterial(nBar + dn, nBar-dn)
 #m = CNC
 s.structures[0].phyParas['m'] = m
 pl.figure(figsize = (3.3,2.8))
-pl.plot(stempcf.wl, (stempcf.spec[:,0])/2-0.025, label = 'Measured')
+pl.plot(stempcf.wl, (stempcf.spec[:, 15*51+15])/2-0.025, label = 'Measured')
 s.setPitch([179.5])
 s.setThickness([2000])
 pl.plot(*s.scanSpectrum(wlRange), label = 'Fit-Best')

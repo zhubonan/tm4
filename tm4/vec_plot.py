54ecd10d10b8cc2471a2181372fa6579bb541255
Bonan Zhu <zhubonan@outlook.com>
2020-02-05 23:07:21 +0000

    Added progress report

--- tm4/vec_plot.py
+++ tm4/vec_plot.py
@@ -7,12 +7,16 @@ Trys to detect blue/red shift
 """
 import scipy as sp
 import numpy as np
-
+from tqdm import tqdm
 
 class GradientArray:
     sample_range = 1
 
     def __init__(self, sdata, start, finish, npoints):
+        """
+        Construct gradient array object using spectrum data.
+        Note that the positive gradient indicates blue shifts
+        """
         self.construct_data(sdata)
         self.spec_start = start
         self.spec_finish = finish
@@ -63,11 +67,17 @@ class GradientArray:
         self.wlrange = sdata.wl
 
     def calc(self):
+        """
+        Compute the shift field
+        """
         n = self.sample_range * 2 + 1
         r = self.sample_range
         shift_store = np.zeros((*self.spec.shape[:2], n, n))
         gradient_row = np.zeros(self.spec.shape[:2])
         gradient_col = np.zeros(self.spec.shape[:2])
+        npoints = (self.spec.shape[0] - 2 * r) * (self.spec.shape[1] - 2 * r)
+        counter = 0
+        tqdm_obj = tqdm(total=npoints)
         for row in range(r, self.spec.shape[0] - r):
             for col in range(r, self.spec.shape[1] - r):
                 shift_matrix = self.calc_shift(row, col)
@@ -75,6 +85,9 @@ class GradientArray:
                 grad = np.gradient(shift_matrix, r)
                 gradient_row[row, col] = grad[0][r, r]
                 gradient_col[row, col] = grad[1][r, r]
+                counter += 1
+                if counter % 10 == 0:
+                    tqdm_obj.update(10)
         self.shift_array = shift_store
         self.gradient_arrays = (gradient_row, gradient_col)
         return shift_store
